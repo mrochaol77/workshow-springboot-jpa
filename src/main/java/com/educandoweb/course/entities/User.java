de3cf20fe868f4c92618ebe03c9b2f67c54069cc
mrochaol <mrochaol@gmail.com>
2025-03-14 17:56:36 -0300

    H2 database, test profile and JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,6 +3,12 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /*
  * Create a new class to entity (ex: User) in .entities package
  * 		Basic entity checklist:
@@ -15,10 +21,14 @@ import java.util.Objects;
  * Create a new resource to a new class entity (ex: UserResource) in .resources package
 */
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
