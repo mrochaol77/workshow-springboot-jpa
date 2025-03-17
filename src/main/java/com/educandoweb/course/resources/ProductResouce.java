7125a5e7434c1436bfafc427a63a7ff3941d09e9
mrochaol <mrochaol@gmail.com>
2025-03-17 15:33:24 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/resources/ProductResouce.java
@@ -0,0 +1,45 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductService;
+
+
+/*
+ * add annotation @RestController
+ * add annotation @RequestMapping with value (ex: class name = Product, value = /users)
+ * 
+ * add annotation @GetMapping to get resources
+ * 
+ * create public ResponseEntity functions to Product class
+ * 
+ * */
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResouce {
+
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = service.findAll();
+		return ResponseEntity.ok().body(list);		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
